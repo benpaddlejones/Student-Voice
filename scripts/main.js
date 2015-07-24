f730e341454ae3cadaa3ab0ce0fb6335c8597ed3
Arian <ajahiri9@gmail.com>
2015-07-24 11:59:51 +1000

    Implemented search function into application.

--- scripts/main.js
+++ scripts/main.js
@@ -9,90 +9,87 @@ $(document).ready(function(){
 });
 
 (function (window, document, undefined) {
-  'use strict'
-
   function Narrower(inp, sel, disp, list) {
-    this.inp  = inp
-    this.sel  = sel
-    this.disp = disp
-    this.list = list
-    this.last = '' // last value on which we narrowed
+    this.inp  = inp;
+    this.sel  = sel;
+    this.disp = disp;
+    this.list = list;
+    this.last = ''; // last value on which we narrowed
   }
   Narrower.prototype = {
     init : function () {
-      this.update('')
-      this.addEvents()
+      this.update('');
+      this.addEvents();
     },
     addEvents : function () {
-      var self
-      self = this
+      var self;
+      self = this;
       this.inp.addEventListener('keyup', function (e) {
         if (this.value !== self.last) {
-          self.last = this.value
-          self.update(this.value)
-        }
-      })
-      this.inp.focus()
+          self.last = this.value;
+          self.update(this.value);
+        };
+      });
+      this.inp.focus();
     },
     update : function (str) {
-      var ulist, rgxp
+      var ulist, rgxp;
       // optimization
       if (0 === str.length) {
-        ulist = this.list
+        ulist = this.list;
       }
       else {
-        ulist = []
+        ulist = [];
         // create rgxp
-        rgxp = new RegExp(str, 'i') // note: not Unicode-safe!
+        rgxp = new RegExp(str, 'i'); // note: not Unicode-safe!
         // keep items that match
         for (var i = this.list.length - 1; i > -1; --i) {
           if (null !== this.list[i].match(rgxp)) {
-            ulist.push(this.list[i])
-          }
-        }
-      }
-      this.updateSelect(ulist.sort())
-      this.updateMatches(ulist.length)
+            ulist.push(this.list[i]);
+          };
+        };
+      };
+      this.updateSelect(ulist.sort());
+      this.updateMatches(ulist.length);
     },
     updateSelect : function (arr) {
-      var self, opts
-      self = this
-      this.sel.options.length = 0
-      opts = this.buildOpts(arr)
+      var self, opts;
+      self = this;
+      this.sel.options.length = 0;
+      opts = this.buildOpts(arr);
       opts.forEach(function (opt, idx) {
-        self.sel.options[idx] = opt
-      })
+        self.sel.options[idx] = opt;
+      });
     },
     buildOpts : function (arr) {
-      var opts
-      opts = []
+      var opts;
+      opts = [];
       arr.forEach(function (val) {
-        opts.push(new Option(val))
-      })
-      return opts
+        opts.push(new Option(val));
+      });
+      return opts;
     },
     updateMatches : function (len) {
-      this.disp.innerHTML = 1 === len ? '1 match' : len + ' matches'
+      this.disp.innerHTML = 1 === len ? '1 match' : len + ' matches';
     }
-  }
+  };
 
   // initialization
-  var inp  = document.querySelector('#nrwr')
-  var sel  = document.querySelector('#names')
-  var disp = document.querySelector('#matches')
+  var inp  = document.querySelector('#nrwr');
+  var sel  = document.querySelector('#names');
+  var disp = document.querySelector('#matches');
 
-  // generate 1000 random names
   var list = (function () {
-    var firsts = ['Merrylands High School', 'Parramatta', 'Granville South', 'Cerdon', 'Sefton', 'Auburn']
-    var lasts  = ['High School', 'High School', 'High School', 'College', 'High School', 'High School']
-    var names  = []
+    var firsts = ['Merrylands High School', 'Parramatta', 'Granville South', 'Cerdon', 'Sefton', 'Auburn'];
+    var lasts  = ['High School', 'High School', 'High School', 'College', 'High School', 'High School'];
+    var names  = [];
     for (var i = 0; i < 6; i++) {
-      names.push(firsts[i])
-    }
-    return names
-  }())
+      names.push(firsts[i]);
+    };
+    return names;
+  }());
 
   // kick it off
-  var nrwr = new Narrower(inp, sel, disp, list)
-  nrwr.init()
-}(this, this.document))
+  var nrwr = new Narrower(inp, sel, disp, list);
+  nrwr.init();
+}(this, this.document));
